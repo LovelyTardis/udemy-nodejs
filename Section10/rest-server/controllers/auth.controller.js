a669427ffd5fea601fc4a09a21251548f2b3826d
Albert <albert.tardis05@gmail.com>
2022-12-15 12:54:00 +0100

    Login completed
    Completed all code in the login function. Validates that the email and password are correct and if the user has his state "true" in the database. Returns code 200 (all ok), 400 (wrong email/password or state is false) or 500 (internal server error or bad connection to mongo database).

--- Section10/rest-server/controllers/auth.controller.js
+++ Section10/rest-server/controllers/auth.controller.js
@@ -1,7 +1,41 @@
 import { request, response } from "express";
+import bcryptjs from "bcryptjs";
+import userExists from "../helpers/userExists.js";
 
-export const loginUser = (req = request, res = response) => {
-  res.json({
-    message: "/login OK",
-  });
+export const loginUser = async (req = request, res = response) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await userExists(email);
+
+    // User exists
+    if (!user) {
+      return res.status(400).json({
+        message: "Wrong email or password",
+      });
+    } else {
+      // Password validator
+      const valid = bcryptjs.compareSync(password, user.password);
+      if (!valid)
+        return res.status(400).json({
+          message: "Wrong email or password",
+        });
+    }
+
+    // User active
+    if (!user.state)
+      return res.status(400).json({
+        message: "User not active",
+      });
+
+    res.json({
+      message: "/login OK",
+      data: user,
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({
+      message: "Internal server error while trying to log in",
+    });
+  }
 };
