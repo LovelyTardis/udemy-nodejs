80311755df66bbff214be69df7af180ed6acc116
Albert <albert.tardis05@gmail.com>
2022-12-17 18:12:34 +0100

    Implemented connect/disconnect

--- Section14/websocket-server/public/js/socket.js
+++ Section14/websocket-server/public/js/socket.js
@@ -1,1 +1,15 @@
 const socket = io();
+const divOnline = document.getElementById("alert-online");
+const divOffline = document.getElementById("alert-offline");
+
+socket.on("connect", () => {
+  divOnline.style.display = "";
+  divOffline.style.display = "none";
+  console.log("Connected");
+});
+
+socket.on("disconnect", () => {
+  divOnline.style.display = "none";
+  divOffline.style.display = "";
+  console.log("DISCONNECTED");
+});
