e4d54cacb25362ae5646fc47c6ab06f118ddffd1
Albert <albert.tardis05@gmail.com>
2022-12-17 14:20:27 +0100

    Update auth.controller.js
    Changed user helpers import.
    Added a creation process if the google user is not in the database.

--- Section11/rest-server/controllers/auth.controller.js
+++ Section11/rest-server/controllers/auth.controller.js
@@ -1,13 +1,14 @@
 import { request, response } from "express";
+
 import generateJwt from "../helpers/generateJwt.js";
-import { userExistsByEmail } from "../helpers/userExists.js";
-import { passwordVerify } from "../helpers/passwordHash.js";
+import { findUserByEmail, createUser } from "../helpers/user/index.js";
+import { passwordHash, passwordVerify } from "../helpers/passwordHash.js";
 
 export const loginUser = async (req = request, res = response) => {
   const { email, password } = req.body;
 
   try {
-    const user = await userExistsByEmail(email);
+    const user = await findUserByEmail(email);
 
     // User exists
     if (!user) {
@@ -46,15 +47,47 @@ export const loginUser = async (req = request, res = response) => {
 };
 
 export const googleSignIn = async (req = request, res = response) => {
-  const user = req.authUser;
+  const authUser = req.authUser;
 
-  if (!user)
+  if (!authUser)
     return res.status(400).json({
       message: "Bad request - no authenticated user",
     });
 
-  res.json({
-    message: "Signed in successfully",
-    user,
-  });
+  const { name, picture, email } = authUser;
+
+  try {
+    let user = await findUserByEmail(email);
+
+    if (!user) {
+      const password = passwordHash("google_no_need_password");
+
+      const userToSave = {
+        name,
+        picture,
+        email,
+        password,
+        google: true,
+      };
+
+      user = await createUser(userToSave);
+    }
+
+    if (!user.state)
+      return res.status(401).json({
+        message: "Not authorized - user is deleted",
+      });
+
+    const token = await generateJwt(user.id);
+
+    res.json({
+      message: "Signed in successfully",
+      token,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      message: "ERROR: Google user could not be authenticated",
+    });
+  }
 };
