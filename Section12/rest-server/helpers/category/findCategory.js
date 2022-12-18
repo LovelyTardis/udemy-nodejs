a97f0349e13020be5ab7e0d1ed03533c0dec26dd
Albert <albert.tardis05@gmail.com>
2022-12-18 13:42:04 +0100

    S12 - Created findAllCategories helper

--- Section12/rest-server/helpers/category/findCategory.js
+++ Section12/rest-server/helpers/category/findCategory.js
@@ -1,5 +1,14 @@
 import { Category } from "../../models/index.js";
 
+export const findAllCategories = async (limit, from) => {
+  const query = { state: true };
+
+  return await Promise.all([
+    Category.countDocuments(),
+    Category.find(query).skip(from).limit(limit),
+  ]);
+};
+
 export const findCategoryByName = async (name) => {
   return await Category.findOne({ name });
 };
