a0c9a70172d70fbce4e57b1dc451bd3a6e25088d
Albert <albert.tardis05@gmail.com>
2022-12-18 15:24:19 +0100

    S12 - Updated validateCategory middleware
    Added a new method for validating the body in the update endpoint.

--- Section12/rest-server/middlewares/validateCategory.js
+++ Section12/rest-server/middlewares/validateCategory.js
@@ -11,7 +11,22 @@ export const validateCategoryIfNotExists = async (
   const category = await Category.findOne({ name });
   if (category)
     return res.status(400).json({
-      message: "Bad request - category already exists.",
+      message: `Bad request - category with name ${name} already exists.`,
+    });
+
+  next();
+};
+
+export const validateCategoryUpdateBody = (
+  req = request,
+  res = response,
+  next
+) => {
+  const { name } = req.body;
+
+  if (!name)
+    return res.status(400).json({
+      message: "Bad request - no name in body",
     });
 
   next();
