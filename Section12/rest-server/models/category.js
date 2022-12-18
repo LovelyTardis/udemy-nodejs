c0ffc20e3ad7800a9426578bfac8274ec25a9ee8
Albert <albert.tardis05@gmail.com>
2022-12-18 14:06:53 +0100

    S12 - Updated toObject method

--- Section12/rest-server/models/category.js
+++ Section12/rest-server/models/category.js
@@ -1,22 +1,32 @@
 import { model, Schema } from "mongoose";
 
-const CategorySchema = Schema({
-  name: {
-    type: String,
-    required: [true, "Name is required"],
-    unique: true,
+const CategorySchema = Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+      unique: true,
+    },
+    state: {
+      type: Boolean,
+      default: true,
+      required: true,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
   },
-  state: {
-    type: Boolean,
-    default: true,
-    required: true,
-  },
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-});
+  {
+    toObject: {
+      transform: function (_, ret) {
+        ret.uid = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
 
 CategorySchema.methods.toJSON = function () {
   const { __v, _id: uid, ...category } = this.toObject();
--- Section12/rest-server/models/user.js
+++ Section12/rest-server/models/user.js
@@ -1,35 +1,45 @@
 import { Schema, model } from "mongoose";
 
-const UserSchema = Schema({
-  name: {
-    type: String,
-    required: [true, "Name is required"],
+const UserSchema = Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Name is required"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    img: {
+      type: String,
+    },
+    role: {
+      type: String,
+      default: "USER_ROLE",
+    },
+    state: {
+      type: Boolean,
+      default: true,
+    },
+    google: {
+      type: Boolean,
+      default: false,
+    },
   },
-  email: {
-    type: String,
-    required: [true, "Email is required"],
-    unique: true,
-  },
-  password: {
-    type: String,
-    required: [true, "Password is required"],
-  },
-  img: {
-    type: String,
-  },
-  role: {
-    type: String,
-    default: "USER_ROLE",
-  },
-  state: {
-    type: Boolean,
-    default: true,
-  },
-  google: {
-    type: Boolean,
-    default: false,
-  },
-});
+  {
+    toObject: {
+      transform: function (_, ret) {
+        ret.uid = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
 
 UserSchema.methods.toJSON = function () {
   const { __v, password, _id: uid, ...user } = this.toObject();
