2459c2ac4e88a0c867fc4436d2c808632b779808
Albert <albert.tardis05@gmail.com>
2022-12-18 11:53:04 +0100

    S12 - New export file for routes files

---
+++ Section12/rest-server/routes/index.js
@@ -0,0 +1,3 @@
+export { default as authRoutes } from "./auth.routes.js";
+export { default as categoriesRoutes } from "./categories.routes.js";
+export { default as userRoutes } from "./user.routes.js";
