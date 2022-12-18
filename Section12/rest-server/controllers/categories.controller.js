e01025b23c0b20c8472e5568aec0dc65d1bbf254
Albert <albert.tardis05@gmail.com>
2022-12-18 12:39:34 +0100

    S12 - Implemented createCategory method

--- Section12/rest-server/controllers/categories.controller.js
+++ Section12/rest-server/controllers/categories.controller.js
@@ -1,4 +1,8 @@
 import { response, request } from "express";
+import {
+  findCategoryByName,
+  createCategory as createCategoryDB,
+} from "../helpers/category/index.js";
 
 export const getCategories = (req = request, res = response) => {
   res.json({
@@ -9,28 +13,49 @@ export const getCategories = (req = request, res = response) => {
 export const getCategory = (req = request, res = response) => {
   const { id } = req.params;
   res.json({
-    message: "get all categories",
+    message: "get category",
   });
 };
 
-export const createCategory = (req = request, res = response) => {
-  const { newUser } = req.body;
+export const createCategory = async (req = request, res = response) => {
+  const name = req.body.name.toUpperCase();
 
-  res.json({
-    message: "get all categories",
+  const category = await findCategoryByName(name);
+
+  if (category) {
+    return res.status(400).json({
+      message: `Category ${category.name} already exists`,
+    });
+  }
+
+  const dataToSave = {
+    name,
+    user: req.user._id,
+  };
+
+  try {
+    await createCategoryDB(dataToSave);
+  } catch (error) {
+    return res.status(500).json({
+      message: `ERROR: ${error}`,
+    });
+  }
+
+  res.status(201).json({
+    message: `New ${category.name} category created`,
   });
 };
 
 export const updateCategory = (req = request, res = response) => {
   const { id } = req.params;
   res.json({
-    message: "get all categories",
+    message: "update category",
   });
 };
 
 export const deleteCategory = (req = request, res = response) => {
   const { id } = req.params;
   res.json({
-    message: "get all categories",
+    message: "delete category",
   });
 };
