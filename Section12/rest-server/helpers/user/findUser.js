13964fba4daa48e27bdf2ef7967164664298e5c5
Albert <albert.tardis05@gmail.com>
2022-12-18 13:45:15 +0100

    S12 - Created findAllUsers helper

--- Section12/rest-server/helpers/user/findUser.js
+++ Section12/rest-server/helpers/user/findUser.js
@@ -1,5 +1,14 @@
 import { User } from "../../models/index.js";
 
+export const findAllUsers = async (limit, from) => {
+  const query = { state: true };
+
+  return await Promise.all([
+    User.countDocuments(),
+    User.find(query).skip(from).limit(limit),
+  ]);
+};
+
 export const findUserByEmail = async (email = "") => {
   return await User.findOne({ email });
 };
