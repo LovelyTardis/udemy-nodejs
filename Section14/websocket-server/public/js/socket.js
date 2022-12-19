b87badadcc2f747ea3bc1287265f81b53fbc892b
Albert <albert.tardis05@gmail.com>
2022-12-19 12:43:40 +0100

    Added and implemented the event listener for the send button

--- Section14/websocket-server/public/js/socket.js
+++ Section14/websocket-server/public/js/socket.js
@@ -2,6 +2,9 @@ const socket = io();
 const divOnline = document.getElementById("alert-online");
 const divOffline = document.getElementById("alert-offline");
 
+const txtMessage = document.getElementById("txt-message");
+const btnSend = document.getElementById("btn-send");
+
 socket.on("connect", () => {
   divOnline.style.display = "";
   divOffline.style.display = "none";
@@ -12,4 +15,13 @@ socket.on("disconnect", () => {
   divOnline.style.display = "none";
   divOffline.style.display = "";
   console.log("DISCONNECTED");
+});
+
+btnSend.addEventListener("click", () => {
+  const payload = {
+    id: socket.id,
+    message: txtMessage.value,
+    date: new Date().getTime(),
+  };
+  socket.emit("sendMessage", payload);
 });
