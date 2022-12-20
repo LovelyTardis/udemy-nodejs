c782ee170a5b770084a6dc727674a94cbe51781a
Albert <albert.tardis05@gmail.com>
2022-12-20 18:48:48 +0100

    S12 - Updated product routes
    Added endpoints.
    Added middlewares.

--- Section12/rest-server/routes/products.routes.js
+++ Section12/rest-server/routes/products.routes.js
@@ -1,12 +1,74 @@
 import { Router } from "express";
 import { check } from "express-validator";
 
+import {
+  getProducts,
+  getProduct,
+  createProduct,
+  updateProduct,
+  deleteProduct,
+} from "../controllers/index.js";
+
+import {
+  validateFields,
+  validateJwt,
+  validateRole,
+  validateProductCreateBody,
+  // validateProductUpdateBody,
+  existsInDatabase,
+  validateProductIfNotExists,
+} from "../middlewares/index.js";
+import { Product } from "../models/index.js";
+
 const productsRoutes = Router();
 
 const middlewares = {
-  getCategory: [],
+  getCategory: [
+    check("id", "Bad request - id is required").not().isEmpty(),
+    check("id", "Bad request - not a valid MongoDB id").isMongoId(),
+    check("id").custom((id) => existsInDatabase(Product, id)),
+    validateFields,
+  ],
+  create: [
+    validateJwt,
+    validateProductIfNotExists,
+    validateProductCreateBody,
+    // validateCategoryInProduct,
+    validateFields,
+  ],
+  update: [
+    validateJwt,
+    check("id", "Bad request - id is required").not().isEmpty(),
+    check("id", "Bad request - not a valid MongoDB id").isMongoId(),
+    check("id").custom((id) => existsInDatabase(Product, id)),
+    validateProductIfNotExists,
+    // validateCategoryInProduct,
+    // validateProductUpdateBody,
+    validateFields,
+  ],
+  delete: [
+    validateJwt,
+    check("id", "Bad request - id is required").not().isEmpty(),
+    check("id", "Bad request - not a valid MongoDB id").isMongoId(),
+    check("id").custom((id) => existsInDatabase(Product, id)),
+    validateRole("ADMIN_ROLE"),
+    validateFields,
+  ],
 };
 
-productsRoutes.get("/");
+// PUBLIC
+productsRoutes.get("/", getProducts);
+
+// PUBLIC
+productsRoutes.get("/:id", middlewares.getCategory, getProduct);
+
+// PRIVATE - HAS VALID TOKEN
+productsRoutes.post("/create", middlewares.create, createProduct);
+
+// PRIVATE - HAS VALID TOKEN
+productsRoutes.put("/update/:id", middlewares.update, updateProduct);
+
+// ADMIN
+productsRoutes.delete("/delete/:id", middlewares.delete, deleteProduct);
 
 export default productsRoutes;
