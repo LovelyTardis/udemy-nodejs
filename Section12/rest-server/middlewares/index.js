73c3384e5ea8853a9002e961f6ea62b2f1ded23f
Albert <albert.tardis05@gmail.com>
2022-12-20 18:42:07 +0100

    S12 - Implemented validateProduct export

--- Section12/rest-server/middlewares/index.js
+++ Section12/rest-server/middlewares/index.js
@@ -3,4 +3,5 @@ export { default as validateJwt } from "./validateJwt.js";
 export { default as validateGoogleToken } from "./validateGoogleToken.js";
 export * from "./validateRoles.js";
 export * from "./validateCategory.js";
+export * from "./validateProduct.js";
 export * from "./dbValidations.js";
