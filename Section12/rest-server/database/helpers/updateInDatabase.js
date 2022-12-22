01f51c1941bd124b64892823c62932a10c71482e
Albert <albert.tardis05@gmail.com>
2022-12-22 18:23:27 +0100

    S12 - Updated update method
    Now can use two populate when updating.

--- Section12/rest-server/database/helpers/updateInDatabase.js
+++ Section12/rest-server/database/helpers/updateInDatabase.js
@@ -2,19 +2,17 @@
  * Updates a document.
  *
  * @param {model} Model mongoose model.
- * @param {object} data {id: String, updatedData: object, populate?: [String]}
+ * @param {object} data {id: String, updatedData: object, populate?: [String], populate2?: [String]}
  * @return {object} updated document.
  */
-export const Update = async (
-  Model,
-  data = {
-    id,
-    updatedData,
-  }
-) => {
-  const { id, updatedData, populate } = data;
+export const Update = async (Model, data) => {
+  const { id, updatedData, populate, populate2 } = data;
 
-  return populate
+  return populate2
+    ? await Model.findByIdAndUpdate(id, updatedData)
+        .populate(...populate)
+        .populate(...populate2)
+    : populate
     ? await Model.findByIdAndUpdate(id, updatedData).populate(...populate)
     : await Model.findByIdAndUpdate(id, updatedData);
 };
