96de147e79d35d3a0c7e56bf420065191d351744
Albert <albert.tardis05@gmail.com>
2022-12-23 17:05:58 +0100

    S12 - Created and updated methods
    Created and implemented a new Find method to get more than one document in a call.
    
    Added populate and populate2 to be null when they are not in the parameters.
    
    Updated FindOne method to implement populate from mongoose.

--- Section12/rest-server/database/helpers/findInDatabase.js
+++ Section12/rest-server/database/helpers/findInDatabase.js
@@ -4,13 +4,13 @@
  * Gets all "state" true documents and can work with limit, from and populate.
  *
  * @param {model} Model mongoose model.
- * @param {object} data {limit?: Number, from?: Number, populate?: [String], populate2?: ["String"]}.
+ * @param {object} data {limit?: Number, from?: Number, populate?, populate2?}.
  *
  * @return {Promise<[Number, Array<model>]>} [total entries, array of found documents].
  */
 export const FindAll = async (Model, data = { limit, from }) => {
   const filter = { state: true };
-  const { limit = 5, from = 0, populate, populate2 } = data;
+  const { limit = 5, from = 0, populate = null, populate2 = null } = data;
 
   return await Promise.all([
     Model.countDocuments(),
@@ -35,12 +35,12 @@ export const FindAll = async (Model, data = { limit, from }) => {
  * It can work with populate.
  *
  * @param {model} Model mongoose model.
- * @param {object} data {id: String, populate?: [String]}.
+ * @param {object} data {id: String, populate?, populate2?}.
  *
  * @return {object} found object.
  */
 export const FindById = async (Model, data) => {
-  const { id, populate, populate2 } = data;
+  const { id, populate = null, populate2 = null } = data;
 
   return populate2
     ? await Model.findById(id)
@@ -55,9 +55,36 @@ export const FindById = async (Model, data) => {
  * Find one document.
  *
  * @param {model} Model mongoose model.
- * @param {object} filter
+ * @param {object} data {filter: object, populate?, populate2?}
  * @return {object} found object.
  */
-export const FindOne = async (Model, filter = {}) => {
-  return await Model.findOne(filter);
+export const FindOne = async (Model, data) => {
+  const { filter, populate = null, populate2 = null } = data;
+
+  return populate2
+    ? await Model.findOne(filter)
+        .populate(...populate)
+        .populate(...populate2)
+    : populate
+    ? await Model.findOne(filter).populate(...populate)
+    : await Model.findOne(filter);
+};
+
+/**
+ * Find documents.
+ *
+ * @param {model} Model mongoose model.
+ * @param {object} data {filter: object, populate?, populate2?}
+ * @return {object} found objects.
+ */
+export const Find = async (Model, data) => {
+  const { filter, populate = null, populate2 = null } = data;
+
+  return populate2
+    ? await Model.find(filter)
+        .populate(...populate)
+        .populate(...populate2)
+    : populate
+    ? await Model.find(filter).populate(...populate)
+    : await Model.find(filter);
 };
