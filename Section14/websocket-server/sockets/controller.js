cbde0b1808413d7f6c0fa0f14e2dce1c689648e6
Albert <albert.tardis05@gmail.com>
2023-01-11 11:37:49 +0100

    Implement new socket events

--- Section14/websocket-server/sockets/controller.js
+++ Section14/websocket-server/sockets/controller.js
@@ -1,14 +1,32 @@
+import TicketControl from "../models/ticket-control.js";
+
+export const ticketControl = new TicketControl();
+
 export const socketController = (client) => {
   console.log("Client " + client.id + " connected");
 
-  client.on("disconnect", () => {
-    console.log("Client " + client.id + " disconnected");
+  client.emit("last-ticket", ticketControl.last);
+  client.emit("actual-state", ticketControl.lastTickets);
+  client.emit("pending-tickets", ticketControl.tickets);
+
+  client.on("next-ticket", (payload, callback) => {
+    const next = ticketControl.next();
+    client.broadcast.emit("pending-tickets", ticketControl.tickets);
+    client.emit("pending-tickets", ticketControl.tickets);
+    callback(next);
   });
 
-  client.on("sendMessage", (payload, callback) => {
-    const { id, message } = payload;
+  client.on("attend-ticket", ({ desk }, callback) => {
+    if (desk == undefined)
+      return callback({ ok: false, msg: "Desk is required" });
 
-    callback(id);
-    client.broadcast.emit("newMessage", `${id}: ${message}`);
+    const ticket = ticketControl.attendTicket(desk);
+
+    client.broadcast.emit("actual-state", ticketControl.lastTickets);
+    client.broadcast.emit("pending-tickets", ticketControl.tickets);
+    client.emit("pending-tickets", ticketControl.tickets);
+
+    if (!ticket) return callback({ ok: false, msg: "No pending tickets" });
+    callback({ ok: true, ticket });
   });
 };
